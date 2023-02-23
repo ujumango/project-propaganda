b40556c5445528c6fc56c648aa305e3d4e12f6c5
Yuju <ujusunrise@gmail.com>
2023-02-24 04:46:50 +0900

    23-02-23 db작업 및 레이아웃 작업

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'database-1.cbqest5qkykx.ap-northeast-2.rds.amazonaws.com',
   user     : 'root',
   password : 'yujoo1020',
-  database : 'yuju',
+  database : 'propa',
   multipleStatements : true //다중쿼리용 설정 -> 테이블 여러개 조회 가능
 });
  
@@ -12,99 +12,62 @@ connection.connect(function(err) {
     console.log("Connected!")
 });
 
-function getNoti(callback){
-    connection.query('SELECT * FROM noticere ORDER BY id desc',(err, rows)=>{
-        if (err) throw err;
-        callback(rows);
-    })
-};
 
 
-function insertNoti(cont, subject, name, cate, callback) {
-    connection.query(`INSERT INTO noticere (create_time, name, cate, title, content) VALUES (NOW(), '${name}','${cate}', '${subject}','${cont}')`, (err)=>{
-        if (err) throw err;
-        callback();
-    })
-};
-
-function getNotibyId(id, callback){
-    connection.query(`SELECT * FROM noticere WHERE id =${id}`,(err,row) => {
-        if(err) throw err;
-        callback(row);
-    })
-}
-
-
-function deleteNotibyId (id, callback){
-    connection.query(`DELETE FROM noticere WHERE id=${id}`,(err)=>{
-        if(err) throw err;
-        callback();
-    })
-}
-
-function updateNotice(id, cont, subject, name, cate, callback){
-    connection.query(`UPDATE noticere SET content='${cont}', title='${subject}', name='${name}', cate='${cate}' WHERE id=${id}`,(err) =>{
-        if(err) throw err;
-        callback();
-    })
-}
-// cate='${cate}'
-
-function memberJoin (name, adress, birth, email, id, pw, callback){
-    connection.query(`INSERT INTO joinre(create_time, email, name, adress, birth, id, pw) values (NOW(),'${email}','${name}','${adress}','${birth}','${id}','${pw}')`, (err) =>{
+function memberJoin (name, birth, email, id, pw, callback){
+    connection.query(`INSERT INTO propajoin (create_time, email, name, birth, id, pw) values (NOW(),'${email}','${name}','${birth}','${id}','${pw}')`, (err) =>{
         if (err) throw err;
         callback();
     })
 }
 
 function loginCheck(id, pw, callback){
-    connection.query(`SELECT * FROM joinre WHERE id='${id}' and pw='${pw}'`, (err, results)=>{
+    connection.query(`SELECT * FROM propajoin WHERE id='${id}' and pw='${pw}'`, (err, results)=>{
         if(err) throw err;
         callback(results);
     })
 }
 
-function insertProduct(img,name,price,text,viewTxt, callback){
-    connection.query(`INSERT INTO productdb(create_time,img,name,price,product_txt,product_exp) VALUES (NOW(), '${img}','${name}','${price}','${text}',${viewTxt}')`,
+function insertBook(img,title,writer,price,info,titleinfo, callback){
+    connection.query(`INSERT INTO propabook(create_time,img,title,writer,price,info,titleinfo) VALUES (NOW(), '${img}', '${title}', '${writer}', '${price}', '${info}', '${titleinfo}')`,
     (err) => {
         if(err) throw err;
         callback();
     })
 }
 
-function getProduct(callback){
-    connection.query('SELECT * FROM productdb ORDER BY id desc', (err,rows,fields)=>{
+function getBook(callback){
+    connection.query('SELECT * FROM propabook ORDER BY id desc', (err,rows,fields)=>{
         if(err) throw err;
         callback(rows);
     })
 }
 
 
-function getProductbyId (id, callback){
-    connection.query(`SELECT * FROM productdb WHERE id = ${id}`, (err,row)=>{
+function getBookbyId (id, callback){
+    connection.query(`SELECT * FROM propabook WHERE id = ${id}`, (err,row)=>{
         if(err) throw err;
         callback(row);
     })
 }
 
-function updateProduct(id,img,name,price,text,viewTxt, callback){
-    connection.query(`UPDATE productdb SET create_time=NOW(),img='${img}', name='${name}',price='${price}', product_txt='${text}', product_exp='${viewTxt}' WHERE id=${id}`, (err)=>{
+function updateBook(id,img,title,writer,price,info,titleinfo, callback){
+    connection.query(`UPDATE propabook SET create_time=NOW(),book_img='${img}', title='${title}',writer='${writer}',price='${price}', info='${info}', titleinfo='${titleinfo}' WHERE id=${id}`, (err)=>{
         if(err) throw err;
         callback();
     })
 }
 
 function deletePbyId (id, callback){
-    connection.query(`DELETE FROM productdb WHERE id=${id}`,(err) => {
+    connection.query(`DELETE FROM propabook WHERE id=${id}`,(err) => {
         if(err) throw err;
         callback();
     })
 }
 
 module.exports = {
-    getNoti, insertNoti,getNotibyId,deleteNotibyId,updateNotice,
     memberJoin,loginCheck,
-    insertProduct,getProduct,
-    getProductbyId,updateProduct,
+    insertBook,getBook,
+    getBookbyId,updateBook,
     deletePbyId
 }
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router(); //R은 대문자! 대소문자 구분해야 함
-const path =require('path');
 const multer = require('multer');
 const fs = require('fs');
-// const db = require('./../db.js');
+const path =require('path');
+const db = require('./../db.js');
 
 
 router.get('/', (req, res)=>{
@@ -35,6 +35,69 @@ router.get('/bookwrite', (req, res)=>{
 
 
 
+ router.post('/bookW',upload.single('book_img'),(req,res)=>{
+  let param = JSON.parse(JSON.stringify(req.body));
+  let img = 'uploads/' + req.file.filename;
+  let title = param ['book_title'];
+  let writer = param ['book_writer'];
+  let price = param ['book_price'];
+  let info = param ['book_txt'];
+  let titleinfo = param ['book_titleinfo'];
+  db.insertBook(img,title,writer,price,info,titleinfo, ()=>{
+    res.redirect('/booklist')
+
+  })
+});
+
+router.get('/booklist', (req, res)=>{
+  db.getBook((rows)=>{
+    res.render('book_list',{rows:rows});
+  })
+
+ });
+
+router.get('/bookBtn',(req,res)=>{
+  let id = req.query.id;
+  console.log(id);
+
+  db.getBookbyId(id,(row) => {
+    res.render('book_update',{row:row[0]})
+  })
+});
+
+router.post('/updateBook', upload.single('book_img'),(req,res)=>{
+  let param = JSON.parse(JSON.stringify(req.body));
+  let img = 'uploads/' + req.file.filename;
+  let title = param ['book_title'];
+  let writer = param ['book_writer'];
+  let price = param ['book_price'];
+  let info = param ['book_txt'];
+  let titleinfo = param ['book_titleinfo'];
+  let id = param['id'];
+
+  db.updateBook(id,img,title,writer,price,info,titleinfo, ()=>{
+    res.redirect('/productlist');
+  })
+});
+
+router.get('/deleteB',(req,res) =>{
+  let id = req.query.id;
+  db.deletePbyId(id,()=>{
+    res.redirect('/booklist')
+  })
+});
+
+
+
+router.get('/bookviewer', (req, res)=>{
+   let id = req.query.id;
+   db.getBookbyId(id, (row) =>{
+    res.render('book_viewer',{row:row[0]})
+   })
+
+ });
+
+
 
 
 
@@ -47,10 +110,11 @@ router.get('/bookwrite', (req, res)=>{
 
 
 //login
-router.get('/login', (req, res)=>{
- res.render('login');
-});
 
+
+router.get('/join',(req, res)=>{
+  res.render('join');
+})
 
 
 
@@ -58,24 +122,38 @@ router.post('/writejoin',(req, res) =>{
   let paramJoin = JSON.parse(JSON.stringify(req.body));
 
   let name = paramJoin['name'];
-  let adress = paramJoin['adress'];
   let birth = paramJoin['birth']
   let id = paramJoin['id'];
+  let email = paramJoin['email'];
   let pw = paramJoin['pw'];
   let repw = paramJoin['repw'];
   console.log(name);
-  console.log(adress);
-  console.log(birth);
-  console.log(id);
-  console.log(pw);
-  console.log(repw);
+
+  db.memberJoin(name, birth, email, id, pw, ()=>{
+    // res.redirect('/');
+    res.send(`<script>alert("${name}님! 회원가입이 완료되었습니다!"); document.location.href="/";</script>`)
+    })
 })
 
-router.get('/join',(req, res)=>{
-  res.render('join');
+router.get('/login', (req, res)=>{
+  res.render('login');
+ });
+
+ router.post('/loginre', (req, res)=>{
+  let param = JSON.parse(JSON.stringify(req.body));
+  let id = param['user_id'];
+  let pw = param['user_pw'];
+
+  db.loginCheck(id,pw, (results)=>{
+    if(results.length>0) {
+      // res.redirect('/')
+      res.send(`<script>alert("${name}님! 환영합니다!"); document.location.href="/";</script>`)
+
+    }else{
+      res.send(`<script>alert("로그인 정보가 일치하지 않습니다."); document.location.href="/login";</script>`)
+    }
+  });
 })
-
-
 
 
 
